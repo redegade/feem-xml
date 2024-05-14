01236745490faa57aa89b568e51ede68fa537f76
Redi Kurti <redikurti.s@gmail.com>
2024-05-13 18:09:47 -0700

    added read and write capabilities to the feem xml itself, removing outside functions

--- src/writeFeemXml.ts
+++
@@ -1,9 +0,0 @@
-import {XMLBuilder} from 'fast-xml-parser';
-import FeemXml from './FeemXml';
-
-export default function writeFeemXml(feemXml: FeemXml): void {
-  const builder = new XMLBuilder();
-  return builder.build({
-    Shipment: feemXml.shipment,
-  });
-}
